092e5facc85ac1ae62f55387917a0b4fd54068f1
BrittaBahr <britta.bahr@outlook.de>
2020-11-11 22:47:17 +0100

    with application inights

--- Lab2/myapp/index.js
+++ Lab2/myapp/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const appInsights = require('applicationinsights');
+appInsights.setup('de60db2f-7d13-4fcc-9ada-7473c3d58a48').start();
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
