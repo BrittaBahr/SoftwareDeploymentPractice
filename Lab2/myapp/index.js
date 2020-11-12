a058a10fef3ee949ca677bdd39761e83895d14be
BrittaBahr <britta.bahr@outlook.de>
2020-11-12 16:18:38 +0100

    chnage port

--- Lab2/myapp/index.js
+++ Lab2/myapp/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
+
 const appInsights = require('applicationinsights');
 appInsights.setup('de60db2f-7d13-4fcc-9ada-7473c3d58a48').start();
 
